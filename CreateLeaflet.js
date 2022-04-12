2a840b551d09355aa3146a916e0a9a2f669b8d1a
jinwa2021 <92895002+jinwa2021@users.noreply.github.com>
2022-04-12 18:15:27 -0400

    Update CreateLeaflet.js

--- CreateLeaflet.js
+++ CreateLeaflet.js
@@ -1,4 +1,4 @@
-var map = L.map('Map_Leaflet').setView([44.14696460408477, -78.448376132845], 13);
+var map = L.map('Map_Leaflet').setView([44.147, -78.448], 13);
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
